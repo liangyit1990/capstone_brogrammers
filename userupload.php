d64309e7f5123ddf1141d714e5664c950d823dcf
Anselm Sim <anselmsim@gmail.com>
2021-07-08 20:38:29 +0800

    nav_logo:hover to black

--- userupload.php
+++ userupload.php
@@ -1,5 +1,10 @@
 <?php
 
+include "config/config.php";
+include "config/db.php";
+include "config/functions.php"; 
+
+
 if(isset($_POST['feedback']) && isset($_FILES['fileToUpload'])) {
     echo "<pre>";
     print_r($_FILES['fileToUpload']);
@@ -8,6 +13,7 @@ if(isset($_POST['feedback']) && isset($_FILES['fileToUpload'])) {
     // $target_dir = "uploads/";
     // $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     
+} else {
     header("Location: " . SITE_URL);
 }
 
