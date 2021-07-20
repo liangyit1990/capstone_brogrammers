4ed301936c2f702e3c21f7d62fd5f4092aafad4c
Anselm Sim <anselmsim@gmail.com>
2021-07-21 05:07:51 +0800

    1

--- menu.php
+++ menu.php
@@ -288,33 +288,33 @@ isAdmin();
 
     // document.getElementById('gravySort').addEventListener('select', sortOption);
 
-    function gravySort () {
-        var value = document.getElementById("gravySort").value;
-        if(value == "gravyalph") {
-            var node = document.createElement("DIV");
-            node.classList.add('col-lg-4');
-            node.classList.add('col-sm-6');
-            node.classList.add('menubox');
-            var textnode = document.createTextNode("<?php 
-                            $gravyalph = DB::query('SELECT * FROM food WHERE food_subcategory = "gravy" ORDER BY food_name');
-                            foreach($gravyalph as $gravyalph_result){
-                                echo '<div class="row"><p class="text-center"><img src=" ';
-                                echo SITE_URL . "admin/" . $gravyalph_result['food_img'];
-                                echo '" alt="" class="menu_img"></p></div><div class="row namerow"><h4 class="text-center">';
-                                echo ucwords($gravyalph_result['food_name']);
-                                echo '</h4></div><div class="row pricerow"><span class="text-center">';
-                                echo "SGD" . $gravyalph_result['food_price'];
-                                echo '</span></div><div class="row caloriesrow"><span class="text-center">';
-                                echo $gravyalph_result['food_calories'] . "Cal";
-                                echo '</span></div><div class="row addbuttonrow"><a class="addbutton text-center" data-id="'.$gravyalph_result['food_id'].'"><i class="bx bxs-cart-download"></i>Add</a></div>';
-                            }
+    // function gravySort () {
+    //     var value = document.getElementById("gravySort").value;
+    //     if(value == "gravyalph") {
+    //         var node = document.createElement("DIV");
+    //         node.classList.add('col-lg-4');
+    //         node.classList.add('col-sm-6');
+    //         node.classList.add('menubox');
+    //         var textnode = document.createTextNode("<?php 
+    //                         $gravyalph = DB::query('SELECT * FROM food WHERE food_subcategory = "gravy" ORDER BY food_name');
+    //                         foreach($gravyalph as $gravyalph_result){
+    //                             echo '<div class="row"><p class="text-center"><img src=" ';
+    //                             echo SITE_URL . "admin/" . $gravyalph_result['food_img'];
+    //                             echo '" alt="" class="menu_img"></p></div><div class="row namerow"><h4 class="text-center">';
+    //                             echo ucwords($gravyalph_result['food_name']);
+    //                             echo '</h4></div><div class="row pricerow"><span class="text-center">';
+    //                             echo "SGD" . $gravyalph_result['food_price'];
+    //                             echo '</span></div><div class="row caloriesrow"><span class="text-center">';
+    //                             echo $gravyalph_result['food_calories'] . "Cal";
+    //                             echo '</span></div><div class="row addbuttonrow"><a class="addbutton text-center" data-id="'.$gravyalph_result['food_id'].'"><i class="bx bxs-cart-download"></i>Add</a></div>';
+    //                         }
             
-            ?>
-");
-            node.appendChild(textnode);
-            document.getElementById("gravydiv").appendChild(node);
-        }
-    }
+    //         ?>
+    //         ");
+    //         node.appendChild(textnode);
+    //         document.getElementById("gravydiv").appendChild(node);
+    //     }
+    // }
 
 </script>
 
