7ece668a97ecd7ae02fcd1062004c8d97400e3ec
Liang Yit <liangyit2@gmail.com>
2021-07-22 00:40:08 +0800

    commit

--- index.php
+++ index.php
@@ -297,32 +297,54 @@ if(isset($_COOKIE["isLoggedIn"]) && (isset($_COOKIE['users_id']))) {
                 <img src="images/bento_aboutus.png" alt="" class="home_img">
             </div>
 
-        </section>
+        </section><hr>
 
 
     </main>
 
     <!-- footer -->
-    <footer class="footer section bd-container">
-        <section class="footer_container bd-grid">
+    <footer class="footer">
+        <section class="footer_container ">
 
             <div class="wave wave1"></div>
             <div class="wave wave2"></div>
             <div class="wave wave3"></div>
             <div class="wave wave4"></div>
-
+                                   
             <div class="footer_content">
-                <a href="index.php" class="footer_logo">CALORICE</a>
-                
-                <div>
-                    <a href="" class="footer_social"><i class='bx bxl-facebook' ></i></a>
-                    <a href="" class="footer_social"><i class='bx bxl-instagram' ></i></a>
-                    <a href="" class="footer_social"><i class='bx bxl-twitter' ></i></a>
-                    
+                <div class="column">                 
+                    <a href="index.php" class="footer_logo">C A L O R I C E</a><br><br>
+                    <p class="copyright">Brogrammers © 2021 - All Rights Reserved</p>
+                </div>
 
+                <div class="column">                 
+                    <h5>Location</h5>
+                    <p>Paya Lebar Square Mall,</p>
+                    <p>L2-130</p>
+                </div>
+                <div class="column">
+                    <h5>Links</h5>
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Menu</a></li>
+                        <li><a href="">Contact Us</a></li>
+                        <li><a href="">Login</a></li>
+                    </ul>
+                </div>
+                <div class= "column">
+                    <h5>Social Media</h5>
+                    <div class="social_icons">
+                        <a href="" class="footer_social"><i class='bx bxl-facebook' ></i></a>
+                        <a href="" class="footer_social"><i class='bx bxl-instagram' ></i></a>
+                        <a href="" class="footer_social"><i class='bx bxl-twitter' ></i></a>
+                    </div>
                 </div> 
-            </div>
+                
+            </div>  
+            
         </section>
+        
+        
     </footer>
     
 <!-- bootstrap jquery -->
