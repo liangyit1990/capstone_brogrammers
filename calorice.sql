9319ec4fc43544ab572535f59897bfc4a97d6368
Anselm Sim <anselmsim@gmail.com>
2021-07-21 20:19:26 +0800

    most updated DB

--- calorice.sql
+++ calorice.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jul 20, 2021 at 11:48 AM
+-- Generation Time: Jul 21, 2021 at 12:19 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -46,25 +46,9 @@ CREATE TABLE `addresses` (
 --
 
 INSERT INTO `addresses` (`addresses_id`, `users_id`, `addresses_default`, `addresses_fullName`, `addresses_companyName`, `addresses_line1`, `addresses_line2`, `addresses_unitNo`, `addresses_country`, `addresses_zipCode`) VALUES
-(28, 19, 0, 'Anselm Sim', '', 'BLK 237 Bukit Panjang Ring Road', '', '#13-75', 'Singapore', '670237'),
-(31, 19, 0, 'Anselm Sim', '', 'BLK 237 Bukit Panjang Ring Road', '', '#13-75', 'Singapore', '670236'),
-(32, 19, 0, 'Anselm Sim', '', 'BLK 237 Bukit Panjang Ring Road', '', '#13-75', 'Singapore', '670236'),
-(33, 19, 0, 'Anselm Sim', '', 'BLK 237 Bukit Panjang Ring Road', '', '#13-75', 'Singapore', '670237'),
-(34, 19, 0, 'Anselm Sim', '', 'BLK 237 Bukit Panjang Ring Road', '', '#13-75', 'Singapore', '670237'),
-(35, 15, 0, 'Kenny', '', 'Test Test Test Address', '', '06-74', 'Singapore', '640757');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bento`
---
-
-CREATE TABLE `bento` (
-  `bento_id` int(11) NOT NULL,
-  `bento_name` varchar(255) NOT NULL,
-  `bento_price` decimal(10,2) NOT NULL,
-  `bento_calories` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+(36, 19, 0, 'Anselm Sim', '', '1st address', 'Gyarados avenue', '#1-1', 'Singapore', '670237'),
+(37, 19, 0, 'Anselm Sim', '', '2nd address', '', '#2-2', 'Singapore', '670237'),
+(38, 19, 1, 'Anselm Sim Test', '', '3rd Address', 'test line 3', '#3-3', 'Singapore', '670237');
 
 -- --------------------------------------------------------
 
@@ -85,23 +69,31 @@ CREATE TABLE `cart` (
 --
 
 INSERT INTO `cart` (`cart_id`, `users_id`, `food_id`, `cart_foodqty`, `cart_status`) VALUES
-(1, 15, 39, 1, 1),
-(2, 15, 38, 1, 1),
-(3, 15, 40, 1, 1),
-(4, 15, 38, 1, 1),
-(5, 15, 40, 1, 1),
-(6, 15, 42, 1, 1),
-(7, 15, 38, 2, 1),
-(8, 15, 43, 1, 1),
-(9, 15, 50, 1, 1),
-(10, 15, 51, 1, 1),
-(11, 15, 48, 1, 1),
-(12, 15, 52, 1, 1),
-(35, 15, 40, 1, 1),
-(36, 15, 39, 1, 1),
-(37, 15, 38, 1, 1),
-(38, 15, 40, 1, 1),
-(39, 15, 51, 1, 1);
+(1, 19, 38, 1, 1),
+(2, 19, 39, 6, 1),
+(3, 19, 40, 4, 1),
+(12, 19, 41, 1, 1),
+(13, 19, 46, 3, 1),
+(14, 19, 48, 1, 1),
+(16, 19, 39, 1, 1),
+(17, 19, 40, 1, 1),
+(18, 19, 51, 1, 1),
+(19, 19, 45, 1, 1),
+(20, 19, 47, 1, 1),
+(22, 19, 38, 1, 1),
+(23, 19, 39, 1, 1),
+(24, 19, 40, 1, 1),
+(25, 19, 43, 1, 1),
+(26, 19, 42, 1, 1),
+(27, 19, 41, 1, 1),
+(28, 19, 45, 1, 1),
+(29, 19, 46, 1, 1),
+(30, 19, 51, 1, 1),
+(31, 19, 47, 1, 1),
+(32, 19, 48, 1, 1),
+(37, 19, 40, 1, 1),
+(38, 19, 46, 1, 1),
+(39, 19, 43, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -117,16 +109,6 @@ CREATE TABLE `cartbatch` (
   `users_id` int(11) NOT NULL,
   `food_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `cartbatch`
---
-
-INSERT INTO `cartbatch` (`cartbatch_id`, `cartbatch_no`, `cartbatch_foodqty`, `cartbatch_status`, `users_id`, `food_id`) VALUES
-(1, 1, 1, 1, 15, 36),
-(6, 1, 1, 1, 15, 36),
-(7, 1, 2, 1, 15, 1),
-(8, 1, 2, 1, 15, 35);
 
 -- --------------------------------------------------------
 
@@ -157,18 +139,22 @@ CREATE TABLE `feedbackothers` (
   `feedbackothers_email` varchar(256) NOT NULL,
   `feedbackothers_topic` varchar(128) NOT NULL,
   `feedbackothers_message` text NOT NULL,
-  `feedbackothers_file` varchar(256) DEFAULT NULL
+  `feedbackothers_file` varchar(256) DEFAULT NULL,
+  `feedbackothers_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `feedbackothers`
 --
 
-INSERT INTO `feedbackothers` (`feedbackothers_id`, `feedbackothers_name`, `feedbackothers_email`, `feedbackothers_topic`, `feedbackothers_message`, `feedbackothers_file`) VALUES
-(1, 'kenny', 'kenny@gmail.com', 'test topic', 'test message', NULL),
-(2, 'kenny', 'tky@gmail.com', 'test msg', 'test msg 2', NULL),
-(4, 'tky', 'tky@gmail.com', 'topic 4', 'topic 4 with attachment', 'submittedfile/100721-soonkueh.jpg'),
-(6, 'kenny', 'kenny@gmail.com', 'topic 6', 'topic 6', 'submittedfile/100721-1-soonkueh.jpg');
+INSERT INTO `feedbackothers` (`feedbackothers_id`, `feedbackothers_name`, `feedbackothers_email`, `feedbackothers_topic`, `feedbackothers_message`, `feedbackothers_file`, `feedbackothers_timestamp`) VALUES
+(1, 'kenny', 'kenny@gmail.com', 'test topic', 'test message', NULL, '2021-07-21 12:18:41'),
+(2, 'kenny', 'tky@gmail.com', 'test msg', 'test msg 2', NULL, '2021-07-21 12:18:41'),
+(3, 'kenny', 'kenny@gmail.com', 'topic 3', 'msg 3', NULL, '2021-07-21 12:18:41'),
+(4, 'tky', 'tky@gmail.com', 'topic 4', 'topic 4 with attachment', 'submittedfile/100721-soonkueh.jpg', '2021-07-21 12:18:41'),
+(5, 'tky', 'tky@gmail.com', 'topic 5', 'msg 5', 'submittedfile/100721-soonkueh.jpg1', '2021-07-21 12:18:41'),
+(6, 'kenny', 'kenny@gmail.com', 'topic 6', 'topic 6', 'submittedfile/100721-1-soonkueh.jpg', '2021-07-21 12:18:41'),
+(7, 'anselm sim', 'anselmsim@gmail.com', 'abc', 'abc', 'submittedfile/150721-oolong_tea-removebg-preview.png', '2021-07-21 12:18:41');
 
 -- --------------------------------------------------------
 
@@ -198,21 +184,22 @@ INSERT INTO `food` (`food_id`, `food_name`, `food_category`, `food_subcategory`,
 (35, 'soy sauce egg', 'ala carte', 'meat', 0, NULL, '1.30', 150, 'uploads/soy_sauce_egg.png'),
 (36, 'rice', 'ala carte', 'base', 0, NULL, '0.50', 130, 'uploads/bento_menu_rice2.png'),
 (37, 'noodle', 'ala carte', 'base', 0, NULL, '0.50', 80, 'uploads/bento_menu_noodles.png'),
-(38, 'Chicken Karaage Bento', 'bento', 'meat', 0, NULL, '12.00', 500, 'uploads/chickenkaraagebento.png'),
-(39, 'Cuttlefish Bento', 'bento', 'meat', 0, NULL, '12.00', 500, 'uploads/cuttlefishbento.png'),
-(40, 'Deep Fried Ham Bento', 'bento', 'meat', 0, NULL, '12.00', 500, 'uploads/deepfriedhambento.png'),
-(41, 'Ebi Fry Bento', 'bento', 'meat', 0, NULL, '12.00', 500, 'uploads/ebifrybento.png'),
-(42, 'Inari Bento', 'bento', 'meat', 0, NULL, '12.00', 500, 'uploads/inaribento.png'),
-(43, 'Salmon Slice Bento', 'bento', 'meat', 0, NULL, '12.00', 500, 'uploads/salmonslicebento.png'),
-(45, 'Sausage Bento', 'bento', 'meat', 0, NULL, '13.99', 501, 'uploads/sausagebento.png'),
+(38, 'chicken karaage bento', 'bento', 'meat', 0, NULL, '11.50', 500, 'uploads/chickenkaraagebento.png'),
+(39, 'cuttlefish bento', 'bento', 'meat', 0, NULL, '12.10', 500, 'uploads/cuttlefishbento.png'),
+(40, 'deep fried ham bento', 'bento', 'meat', 0, NULL, '10.90', 500, 'uploads/deepfriedhambento.png'),
+(41, 'ebi fry bento', 'bento', 'meat', 0, NULL, '11.60', 500, 'uploads/ebifrybento.png'),
+(42, 'inari bento', 'bento', 'meat', 0, NULL, '12.50', 500, 'uploads/inaribento.png'),
+(43, 'salmon slice bento', 'bento', 'meat', 0, NULL, '12.40', 500, 'uploads/salmonslicebento.png'),
+(45, 'sausage bento', 'bento', 'meat', 0, NULL, '13.50', 501, 'uploads/sausagebento.png'),
 (46, 'carrot juice', 'ala carte', 'drinks', 0, NULL, '2.50', 150, 'uploads/carrotjuice.png'),
 (47, 'curry gravy', 'ala carte', 'gravy', 0, NULL, '2.99', 300, 'uploads/gravycurry.png'),
 (48, 'braised soy gravy', 'ala carte', 'gravy', 0, NULL, '2.29', 250, 'uploads/gravybraisedsoy.png'),
 (49, 'mushroom gravy', 'ala carte', 'gravy', 0, NULL, '4.99', 375, 'uploads/gravymushroom.png'),
-(50, 'lemon ginger green tea', 'ala carte', 'drinks', 0, NULL, '2.99', 45, 'uploads/lemon_ginger_green_tea-removebg-preview.png'),
-(51, 'black tea', 'ala carte', 'drinks', 0, NULL, '2.99', 35, 'uploads/black_tea-removebg-preview.png'),
-(52, 'oolong tea', 'ala carte', 'drinks', 0, NULL, '1.99', 60, 'uploads/oolong_tea-removebg-preview.png'),
-(53, 'red tea', 'ala carte', 'drinks', 0, NULL, '1.99', 77, 'uploads/red_tea-removebg-preview.png');
+(50, 'lemon ginger green tea', 'ala carte', 'drinks', 0, NULL, '1.99', 45, 'uploads/lemon_ginger_green_tea-removebg-preview.png'),
+(51, 'black tea', 'ala carte', 'drinks', 0, NULL, '2.40', 35, 'uploads/black_tea-removebg-preview.png'),
+(52, 'oolong tea', 'ala carte', 'drinks', 0, NULL, '1.49', 60, 'uploads/oolong_tea-removebg-preview.png'),
+(53, 'red tea', 'ala carte', 'drinks', 0, NULL, '1.99', 77, 'uploads/red_tea-removebg-preview.png'),
+(54, 'teriyaki chicken bento', 'bento', 'meat', 0, NULL, '15.50', 600, 'uploads/Teriyaki Chicken Bento.png');
 
 -- --------------------------------------------------------
 
@@ -234,27 +221,30 @@ CREATE TABLE `orderdetails` (
 --
 
 INSERT INTO `orderdetails` (`orderdetails_id`, `orderdetails_qty`, `orderdetails_group`, `users_id`, `food_id`, `orders_id`) VALUES
-(1, 1, 1, 15, 39, 1),
-(2, 1, 2, 15, 38, 1),
-(3, 1, 3, 15, 40, 1),
-(4, 1, 1, 15, 36, 2),
-(5, 1, 2, 15, 38, 2),
-(6, 1, 3, 15, 40, 2),
-(7, 1, 4, 15, 42, 2),
-(8, 2, 1, 15, 38, 3),
-(9, 1, 2, 15, 43, 3),
-(10, 1, 3, 15, 50, 3),
-(11, 1, 4, 15, 51, 3),
-(12, 1, 5, 15, 48, 3),
-(13, 1, 6, 15, 52, 3),
-(14, 1, 1, 15, 40, 4),
-(15, 1, 2, 15, 39, 4),
-(16, 1, 3, 15, 38, 4),
-(17, 1, 1, 15, 36, 5),
-(18, 2, 1, 15, 1, 5),
-(19, 2, 1, 15, 35, 5),
-(20, 1, 2, 15, 40, 5),
-(21, 1, 3, 15, 51, 5);
+(1, 1, 1, 19, 38, 1),
+(2, 1, 2, 19, 39, 1),
+(3, 1, 3, 19, 40, 1),
+(4, 1, 1, 19, 41, 2),
+(5, 1, 2, 19, 46, 2),
+(6, 1, 1, 19, 48, 3),
+(7, 1, 1, 19, 39, 4),
+(8, 1, 2, 19, 40, 4),
+(9, 1, 3, 19, 51, 4),
+(10, 1, 4, 19, 45, 4),
+(11, 1, 5, 19, 47, 4),
+(12, 1, 1, 19, 38, 5),
+(13, 1, 2, 19, 39, 5),
+(14, 1, 3, 19, 40, 5),
+(15, 1, 4, 19, 43, 5),
+(16, 1, 5, 19, 42, 5),
+(17, 1, 6, 19, 41, 5),
+(18, 1, 7, 19, 45, 5),
+(19, 1, 8, 19, 46, 5),
+(20, 1, 9, 19, 51, 5),
+(21, 1, 10, 19, 47, 5),
+(22, 1, 11, 19, 48, 5),
+(23, 1, 1, 19, 40, 6),
+(24, 1, 1, 19, 46, 7);
 
 -- --------------------------------------------------------
 
@@ -266,6 +256,7 @@ CREATE TABLE `orders` (
   `orders_id` int(11) NOT NULL,
   `orders_totalprice` decimal(10,2) NOT NULL,
   `orders_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `orders_address` varchar(256) NOT NULL,
   `users_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -273,12 +264,14 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`orders_id`, `orders_totalprice`, `orders_timestamp`, `users_id`) VALUES
-(1, '36.00', '2021-07-19 14:29:59', 15),
-(2, '36.50', '2021-07-20 02:42:35', 15),
-(3, '46.26', '2021-07-20 02:43:09', 15),
-(4, '36.00', '2021-07-20 05:39:40', 15),
-(5, '28.07', '2021-07-20 05:40:40', 15);
+INSERT INTO `orders` (`orders_id`, `orders_totalprice`, `orders_timestamp`, `orders_address`, `users_id`) VALUES
+(1, '34.50', '2021-07-19 13:43:46', '', 19),
+(2, '14.10', '2021-07-19 14:08:41', '', 19),
+(3, '2.29', '2021-07-19 14:15:14', '', 19),
+(4, '41.89', '2021-07-19 14:30:50', '', 19),
+(5, '94.68', '2021-07-19 14:32:33', '', 19),
+(6, '10.90', '2021-07-21 10:49:44', '3rd Address Test Line 3 #3-3 Singapore ', 19),
+(7, '2.50', '2021-07-21 10:53:51', '2nd address  #2-2 Singapore 670237', 19);
 
 -- --------------------------------------------------------
 
@@ -317,10 +310,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`users_id`, `users_name`, `users_email`, `users_password`, `users_address`, `users_phone`, `users_age`, `users_gender`, `users_permission`, `users_joindate`) VALUES
-(6, 'kys', 'ky@gmail.com', '$2y$10$HtGb5WNwO6SBkoY2B/bZOe6QVePW.8ywqgMhmI8LeTV67zy3pQyGu', '', '', NULL, 0, 1, '2021-07-20 06:08:18'),
+(6, 'ky', 'ky@gmail.com', '$2y$10$HtGb5WNwO6SBkoY2B/bZOe6QVePW.8ywqgMhmI8LeTV67zy3pQyGu', '', '', NULL, 0, 1, '2021-07-06 08:31:17'),
 (8, 'hy', 'hy@gmail.com', '$2y$10$93fTm7vsxDe8B1WY/1w/YO2j36KvcHEnlRy..VYXy02gxWy5fjgVy', NULL, NULL, NULL, NULL, 0, '2021-07-08 11:45:23'),
 (15, 'tky', 'tky@gmail.com', '$2y$10$CpUgeO/P9NEE4rComYRp7ObDZLkwCe6vAtYP8WijdlCfLPnVWesgu', '', '', NULL, 0, 0, '2021-07-09 10:59:02'),
-(16, 'kenny', 'kenny@outlook.com', '$2y$10$EtHv/A7Sx1EKJlnh8JefyO5wv443goMi5nEfFAWm1dSByiGzZw2mm', NULL, NULL, NULL, NULL, 0, '2021-07-10 08:34:49');
+(16, 'kenny', 'kenny@outlook.com', '$2y$10$EtHv/A7Sx1EKJlnh8JefyO5wv443goMi5nEfFAWm1dSByiGzZw2mm', NULL, NULL, NULL, NULL, 0, '2021-07-10 08:34:49'),
+(17, 'james', 'james@james.com', '$2y$10$fV.Gr2sveaRd0asxWJ7Us..eM9skXH.tYLXHV7jrc76eFtjV2X23u', NULL, NULL, NULL, NULL, 0, '2021-07-13 10:40:25'),
+(19, 'anselm sim', 'anselmsim@gmail.com', '$2y$10$KcO0sXOqhRcqwhvR43RR3uVG5Z2dUJlH83GB64Yj6sx3ucgrcyOE.', '', '', NULL, 1, 0, '2021-07-19 15:26:19');
 
 -- --------------------------------------------------------
 
@@ -347,12 +342,6 @@ CREATE TABLE `vouchers` (
 --
 ALTER TABLE `addresses`
   ADD PRIMARY KEY (`addresses_id`);
-
---
--- Indexes for table `bento`
---
-ALTER TABLE `bento`
-  ADD PRIMARY KEY (`bento_id`);
 
 --
 -- Indexes for table `cart`
@@ -433,13 +422,7 @@ ALTER TABLE `vouchers`
 -- AUTO_INCREMENT for table `addresses`
 --
 ALTER TABLE `addresses`
-  MODIFY `addresses_id` int(128) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
-
---
--- AUTO_INCREMENT for table `bento`
---
-ALTER TABLE `bento`
-  MODIFY `bento_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `addresses_id` int(128) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `cart`
@@ -451,7 +434,7 @@ ALTER TABLE `cart`
 -- AUTO_INCREMENT for table `cartbatch`
 --
 ALTER TABLE `cartbatch`
-  MODIFY `cartbatch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `cartbatch_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `feedback`
@@ -469,19 +452,19 @@ ALTER TABLE `feedbackothers`
 -- AUTO_INCREMENT for table `food`
 --
 ALTER TABLE `food`
-  MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `orderdetails`
 --
 ALTER TABLE `orderdetails`
-  MODIFY `orderdetails_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `orderdetails_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `orders_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `orders_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `submission`
@@ -493,7 +476,7 @@ ALTER TABLE `submission`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `vouchers`
