9781fcee53080a9803c3c16db981ca38503fe510
Kenny Tay Kian Yick <taykianyick@outlook.com>
2021-07-21 21:36:00 +0800

    Updates on DB

--- calorice.sql
+++ calorice.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jul 21, 2021 at 12:19 PM
+-- Generation Time: Jul 21, 2021 at 01:34 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -48,7 +48,8 @@ CREATE TABLE `addresses` (
 INSERT INTO `addresses` (`addresses_id`, `users_id`, `addresses_default`, `addresses_fullName`, `addresses_companyName`, `addresses_line1`, `addresses_line2`, `addresses_unitNo`, `addresses_country`, `addresses_zipCode`) VALUES
 (36, 19, 0, 'Anselm Sim', '', '1st address', 'Gyarados avenue', '#1-1', 'Singapore', '670237'),
 (37, 19, 0, 'Anselm Sim', '', '2nd address', '', '#2-2', 'Singapore', '670237'),
-(38, 19, 1, 'Anselm Sim Test', '', '3rd Address', 'test line 3', '#3-3', 'Singapore', '670237');
+(38, 19, 1, 'Anselm Sim Test', '', '3rd Address', 'test line 3', '#3-3', 'Singapore', '670237'),
+(39, 15, 1, 'Kenny', '', 'Test Test Test Address', '', '06-74', 'Singapore', '640757');
 
 -- --------------------------------------------------------
 
@@ -93,7 +94,9 @@ INSERT INTO `cart` (`cart_id`, `users_id`, `food_id`, `cart_foodqty`, `cart_stat
 (32, 19, 48, 1, 1),
 (37, 19, 40, 1, 1),
 (38, 19, 46, 1, 1),
-(39, 19, 43, 1, 0);
+(39, 19, 43, 1, 0),
+(48, 15, 39, 1, 0),
+(49, 15, 40, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -109,6 +112,13 @@ CREATE TABLE `cartbatch` (
   `users_id` int(11) NOT NULL,
   `food_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cartbatch`
+--
+
+INSERT INTO `cartbatch` (`cartbatch_id`, `cartbatch_no`, `cartbatch_foodqty`, `cartbatch_status`, `users_id`, `food_id`) VALUES
+(1, 1, 1, 1, 15, 36);
 
 -- --------------------------------------------------------
 
@@ -154,7 +164,8 @@ INSERT INTO `feedbackothers` (`feedbackothers_id`, `feedbackothers_name`, `feedb
 (4, 'tky', 'tky@gmail.com', 'topic 4', 'topic 4 with attachment', 'submittedfile/100721-soonkueh.jpg', '2021-07-21 12:18:41'),
 (5, 'tky', 'tky@gmail.com', 'topic 5', 'msg 5', 'submittedfile/100721-soonkueh.jpg1', '2021-07-21 12:18:41'),
 (6, 'kenny', 'kenny@gmail.com', 'topic 6', 'topic 6', 'submittedfile/100721-1-soonkueh.jpg', '2021-07-21 12:18:41'),
-(7, 'anselm sim', 'anselmsim@gmail.com', 'abc', 'abc', 'submittedfile/150721-oolong_tea-removebg-preview.png', '2021-07-21 12:18:41');
+(7, 'anselm sim', 'anselmsim@gmail.com', 'abc', 'abc', 'submittedfile/150721-oolong_tea-removebg-preview.png', '2021-07-21 12:18:41'),
+(8, 'tky', 'tky@gmail.com', 'test new topic with timestamp', 'new message with timestamp', NULL, '2021-07-21 12:24:20');
 
 -- --------------------------------------------------------
 
@@ -244,7 +255,8 @@ INSERT INTO `orderdetails` (`orderdetails_id`, `orderdetails_qty`, `orderdetails
 (21, 1, 10, 19, 47, 5),
 (22, 1, 11, 19, 48, 5),
 (23, 1, 1, 19, 40, 6),
-(24, 1, 1, 19, 46, 7);
+(24, 1, 1, 19, 46, 7),
+(25, 1, 1, 15, 36, 8);
 
 -- --------------------------------------------------------
 
@@ -271,7 +283,8 @@ INSERT INTO `orders` (`orders_id`, `orders_totalprice`, `orders_timestamp`, `ord
 (4, '41.89', '2021-07-19 14:30:50', '', 19),
 (5, '94.68', '2021-07-19 14:32:33', '', 19),
 (6, '10.90', '2021-07-21 10:49:44', '3rd Address Test Line 3 #3-3 Singapore ', 19),
-(7, '2.50', '2021-07-21 10:53:51', '2nd address  #2-2 Singapore 670237', 19);
+(7, '2.50', '2021-07-21 10:53:51', '2nd address  #2-2 Singapore 670237', 19),
+(8, '0.50', '2021-07-21 12:49:33', ',  ,  ', 15);
 
 -- --------------------------------------------------------
 
@@ -297,7 +310,6 @@ CREATE TABLE `users` (
   `users_name` varchar(255) NOT NULL,
   `users_email` varchar(255) NOT NULL,
   `users_password` varchar(128) NOT NULL,
-  `users_address` text,
   `users_phone` varchar(11) DEFAULT NULL,
   `users_age` int(3) DEFAULT NULL,
   `users_gender` smallint(6) DEFAULT NULL,
@@ -309,13 +321,13 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`users_id`, `users_name`, `users_email`, `users_password`, `users_address`, `users_phone`, `users_age`, `users_gender`, `users_permission`, `users_joindate`) VALUES
-(6, 'ky', 'ky@gmail.com', '$2y$10$HtGb5WNwO6SBkoY2B/bZOe6QVePW.8ywqgMhmI8LeTV67zy3pQyGu', '', '', NULL, 0, 1, '2021-07-06 08:31:17'),
-(8, 'hy', 'hy@gmail.com', '$2y$10$93fTm7vsxDe8B1WY/1w/YO2j36KvcHEnlRy..VYXy02gxWy5fjgVy', NULL, NULL, NULL, NULL, 0, '2021-07-08 11:45:23'),
-(15, 'tky', 'tky@gmail.com', '$2y$10$CpUgeO/P9NEE4rComYRp7ObDZLkwCe6vAtYP8WijdlCfLPnVWesgu', '', '', NULL, 0, 0, '2021-07-09 10:59:02'),
-(16, 'kenny', 'kenny@outlook.com', '$2y$10$EtHv/A7Sx1EKJlnh8JefyO5wv443goMi5nEfFAWm1dSByiGzZw2mm', NULL, NULL, NULL, NULL, 0, '2021-07-10 08:34:49'),
-(17, 'james', 'james@james.com', '$2y$10$fV.Gr2sveaRd0asxWJ7Us..eM9skXH.tYLXHV7jrc76eFtjV2X23u', NULL, NULL, NULL, NULL, 0, '2021-07-13 10:40:25'),
-(19, 'anselm sim', 'anselmsim@gmail.com', '$2y$10$KcO0sXOqhRcqwhvR43RR3uVG5Z2dUJlH83GB64Yj6sx3ucgrcyOE.', '', '', NULL, 1, 0, '2021-07-19 15:26:19');
+INSERT INTO `users` (`users_id`, `users_name`, `users_email`, `users_password`, `users_phone`, `users_age`, `users_gender`, `users_permission`, `users_joindate`) VALUES
+(6, 'ky', 'ky@gmail.com', '$2y$10$HtGb5WNwO6SBkoY2B/bZOe6QVePW.8ywqgMhmI8LeTV67zy3pQyGu', '91869647', NULL, 1, 1, '2021-07-21 13:17:32'),
+(8, 'hy', 'hy@gmail.com', '$2y$10$93fTm7vsxDe8B1WY/1w/YO2j36KvcHEnlRy..VYXy02gxWy5fjgVy', NULL, NULL, NULL, 0, '2021-07-08 11:45:23'),
+(15, 'tky', 'tky@gmail.com', '$2y$10$CpUgeO/P9NEE4rComYRp7ObDZLkwCe6vAtYP8WijdlCfLPnVWesgu', '', NULL, 0, 0, '2021-07-09 10:59:02'),
+(16, 'kenny', 'kenny@outlook.com', '$2y$10$EtHv/A7Sx1EKJlnh8JefyO5wv443goMi5nEfFAWm1dSByiGzZw2mm', NULL, NULL, NULL, 0, '2021-07-10 08:34:49'),
+(17, 'james', 'james@james.com', '$2y$10$fV.Gr2sveaRd0asxWJ7Us..eM9skXH.tYLXHV7jrc76eFtjV2X23u', NULL, NULL, NULL, 0, '2021-07-13 10:40:25'),
+(19, 'anselm sim', 'anselmsim@gmail.com', '$2y$10$KcO0sXOqhRcqwhvR43RR3uVG5Z2dUJlH83GB64Yj6sx3ucgrcyOE.', '', NULL, 1, 0, '2021-07-19 15:26:19');
 
 -- --------------------------------------------------------
 
@@ -341,7 +353,8 @@ CREATE TABLE `vouchers` (
 -- Indexes for table `addresses`
 --
 ALTER TABLE `addresses`
-  ADD PRIMARY KEY (`addresses_id`);
+  ADD PRIMARY KEY (`addresses_id`),
+  ADD KEY `users_id` (`users_id`);
 
 --
 -- Indexes for table `cart`
@@ -422,19 +435,19 @@ ALTER TABLE `vouchers`
 -- AUTO_INCREMENT for table `addresses`
 --
 ALTER TABLE `addresses`
-  MODIFY `addresses_id` int(128) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `addresses_id` int(128) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `cartbatch`
 --
 ALTER TABLE `cartbatch`
-  MODIFY `cartbatch_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cartbatch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `feedback`
@@ -446,7 +459,7 @@ ALTER TABLE `feedback`
 -- AUTO_INCREMENT for table `feedbackothers`
 --
 ALTER TABLE `feedbackothers`
-  MODIFY `feedbackothers_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `feedbackothers_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `food`
@@ -458,13 +471,13 @@ ALTER TABLE `food`
 -- AUTO_INCREMENT for table `orderdetails`
 --
 ALTER TABLE `orderdetails`
-  MODIFY `orderdetails_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `orderdetails_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `orders_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `orders_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `submission`
@@ -487,6 +500,12 @@ ALTER TABLE `vouchers`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `addresses`
+--
+ALTER TABLE `addresses`
+  ADD CONSTRAINT `addresses_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`users_id`);
 
 --
 -- Constraints for table `cart`
