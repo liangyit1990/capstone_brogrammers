4f8d7449045f2c4cab5311c7fd85136be4725330
Anselm Sim <anselmsim@gmail.com>
2021-07-23 18:56:44 +0800

    ajax done for registering and logging in

--- register.php
+++ register.php
@@ -1,114 +1,62 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login/Register</title>
-    <link rel="stylesheet" href="css/register.css" type="text/css">
-    <!-- for icon -->
-    <link href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" rel="stylesheet">
-    <!-- fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Cedarville+Cursive&display=swap" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
-    
-</head>
-<body> 
-    <!-- <div class="hero">
-        <div class="form-box">
-            <div class="button-box"> 
-                <div id="btn"></div>
-                <button type="button" class="toggle-btn" onclick="login()">Sign In</button>
-                <button type="button" class="toggle-btn" onclick="register()">Register</button>
-            </div>
-            <form id="login" class="input-group" name="submission">
-                <input type="text" class="input-field" id="username" placeholder="Username" required>
-                <input type="password" class="input-field" id="password" placeholder="Password" required>
-                <input type="checkbox" class="check-box"><span>Remember Password</span>
-                <button type="button" class="submit-btn" onclick="getInfo()">Sign in</button>
-                
-            </form>
-            <h6><a href="index.html">back to home</a></h6>
-            <form id="register" class="input-group">
-                <input type="text" class="input-field" placeholder="Username" required>
-                <input type="email" class="input-field" placeholder="Email" required>
-                <input type="password" class="input-field" placeholder="Enter Password" required>
-                <input type="checkbox" class="check-box"><span>I agree to the T&C</span>
-                <button type="submit" class="submit-btn">Register</button>
-            </form>
-            
-        </div>
-        
-    </div> -->
-    <!-- Modal -->
-    <!-- id is linked to data-bs-target value of the button-->
-    <div class="modal fade" id="register" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="register">Login/Register</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-            
-        <div class="hero">
-        <div class="form-box">
-            <div class="button-box"> 
-                <div id="btn"></div>
-                <button type="button" class="toggle-btn" onclick="login()">Sign In</button>
-                <button type="button" class="toggle-btn" onclick="register()">Register</button>
-            </div>
-            <form id="login" class="input-group" name="submission">
-                <input type="text" class="input-field" id="username" placeholder="Username" required>
-                <input type="password" class="input-field" id="password" placeholder="Password" required>
-                <input type="checkbox" class="check-box"><span class="span">Remember Password</span>
-                <button type="button" class="submit-btn" onclick="getInfo()">Sign in</button>
-                
-            </form>
-            <h6><a href="index.html">back to home</a></h6>
-            <form id="register" class="input-group">
-                <input type="text" class="input-field" placeholder="Username" required>
-                <input type="email" class="input-field" placeholder="Email" required>
-                <input type="password" class="input-field" placeholder="Enter Password" required>
-                <input type="checkbox" class="check-box"><span class="span">I agree to the T&C</span>
-                <button type="submit" class="submit-btn">Register</button>
-            </form>
-            
-        </div>
-        
-    </div>
+<?php
+include "config/config.php";
+include "config/db.php";
+include "config/functions.php"; 
 
+// Start of Register PHP Code
+if(isset($_POST['register'])){
+    if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['password']) || empty($_POST['cfmPassword'])){
+      echo "Please fill up all the fields.";
+      // echo $registererror;
+    }else{
+      $email = validateData($_POST['email']);
+      //validate if email is legit
+      if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $name = validateData($_POST['name']);
+        $password = validateData($_POST['password']);
+        $cfmPassword = validateData($_POST['cfmPassword']);
+        //check if password and confirm password are the same
+        if($password == $cfmPassword){
+          //check if email exist in database
+          $checkEmail = DB::query("SELECT * FROM users WHERE users_email=%s AND users_permission=0", $email);
+          $checkEmailCount = DB::count();
+          //if email already exist
+          if($checkEmailCount > 0){
+            echo "The email you entered has already been used! Please try another email.";
+          }else{
+              DB::insert("users", [
+                'users_name' => $name,
+                'users_email' => $email,
+                'users_password' => password_hash($password, PASSWORD_DEFAULT),
+                // 'users_registeredDate' => MySQLDate($date)
+              ]);
+  
+              
+              $userQuery = DB::query("SELECT * FROM users WHERE users_email=%s AND users_permission=0", $email);
+              foreach($userQuery as $userResult) {
+              $dbId = $userResult['users_id'];
+              $dbPermission = $userResult['users_permission'];
+              $dbName = $userResult['users_name'];
+              $dbEmail = $userResult['users_email'];
+              }
+  
+              setcookie("users_id", $dbId, time() + (86400 * 30)); // 86400 = 1 day
+              setcookie("users_permission", $dbPermission, time() + (86400 * 30)); // 86400 = 1 day
+              setcookie("users_name", $dbName, time() + (86400 * 30)); // 86400 = 1 day
+              setcookie("users_email", $dbEmail, time() + (86400 * 30)); // 86400 = 1 day
+              setcookie("isLoggedIn", 1, time() + (86400 * 30)); // 86400 = 1 day
+              
+  
+              echo 1;
+          }
+        }else{
+          echo "Your passwords do not match! Please try again";
+        }
+      } else{
+        echo "Please enter a valid email.";
+      }
+    }
+  }
+  
 
-            </div>
-            <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-        </div>
-        </div>
-    </div>
-    <!-- <div class="modal fade" id="noodlecart" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-            ...
-            </div>
-            <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-        </div>
-        </div>
-    </div> -->
-    <script src="register.js"></script>
-
-</body>
-</html>
+?>
